d9e7693dc72dd680fb45191cf7f7f22e0300bd38
Patrick Putman <putman.patrick@gmail.com>
2011-09-26 20:50:45 -0500

    Added rails/refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DavisEyecare::Application.config.secret_token = 'a6e049be5b266844bb643ec3c1a3c1c8530cc6e337ac343f020dd6b47c545f9a2f544689b437481147fbbac511b1cacbd9b72e557b6b243caefae22011224d08'
