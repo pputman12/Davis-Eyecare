c7f2f8e143040c7ebae88075ba9714927db465a6
Patrick Putman <putman.patrick@gmail.com>
2011-09-26 20:49:09 -0500

    CLEAR

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DavisEyecare::Application.config.secret_token = 'c210666a06b7daa3349c2d6b1347fcb117ff7dba71d1d404f959ada679a70e60c5de145d009ae1d69407bd9f7e414928798df1f53a4e5be11652f2ecd4f6dd71'
